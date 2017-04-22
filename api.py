2cb7b28930e6207dc722602a96cea274124af847
Oleg Gromyak <oagromyak@gmail.com>
2017-04-22 18:42:51 +0300

    Add api.vk.com/messages.send

--- api.py
+++ api.py
@@ -1,4 +1,22 @@
+import requests
+import constants as c
+
+
 class VkApi:
     @staticmethod
     def send_msg(user, data):
-        pass
+        url = 'https://api.vk.com/method/messages.send'
+        post_data = {'access_token': c.VK_TOKEN, 'user_id': user, 'attachment': []}
+
+        if type(data) is str:
+            post_data['message'] = data
+        elif 'message' in data:
+            post_data['message'] = data['text']
+
+        for attachment in ['photo', 'video', 'audio', 'doc', 'wall', 'market']:
+            if attachment in data:
+                post_data['attachment'].append(data[attachment])
+        post_data['attachment'] = ','.join(post_data['attachment'])
+
+        r = requests.post(url, data=post_data)
+        # TODO: check r.status
