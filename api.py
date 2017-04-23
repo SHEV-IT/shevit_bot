f46df513711c5bc6f86e1f7847730c394a500ff5
Oleg Gromyak <oagromyak@gmail.com>
2017-04-23 16:39:58 +0300

    Fix attachments bug in VkApi

--- api.py
+++ api.py
@@ -8,15 +8,14 @@ class VkApi:
         url = 'https://api.vk.com/method/messages.send'
         post_data = {'access_token': c.VK_TOKEN, 'user_id': user, 'attachment': []}
 
-        if type(data) is str:
+        if type(data) is str or type(data) is unicode:
             post_data['message'] = data
         elif 'message' in data:
             post_data['message'] = data['text']
-
-        for attachment in ['photo', 'video', 'audio', 'doc', 'wall', 'market']:
-            if attachment in data:
-                post_data['attachment'].append(data[attachment])
-        post_data['attachment'] = ','.join(post_data['attachment'])
+            for attachment in ['photo', 'video', 'audio', 'doc', 'wall', 'market']:
+                if attachment in data:
+                    post_data['attachment'].append(data[attachment])
+            post_data['attachment'] = ','.join(post_data['attachment'])
 
         r = requests.post(url, data=post_data)
         # TODO: check r.status
